9195dbfc2087e986cb14e0e159826679ae7e4178
john_wisneski <john.j.wisneski@gmail.com>
2021-02-08 11:22:24 -0500

    all toys on screen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-let addToy = false;
+// let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const url = 'http://localhost:3007/toys'
   const addBtn = document.querySelector("#new-toy-btn");
+  const toyCollection = document.querySelector("div#toy-collection");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -12,4 +14,30 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  function renderAllToys(){
+    fetch(url)
+      .then(response => response.json())
+      .then(toys => toys.forEach(toy => renderOneToy(toy)))
+  }
+
+  function renderOneToy(toy){
+    const card = document.createElement('div')
+    // card.setAttribute('toy-card', toy.id)
+    card.className = 'toy-card'
+    card.dataset.id = toy.id 
+
+    card.innerHTML = `
+    <div class="card">
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>4 Likes: ${toy.likes}  </p>
+    <button class="like-btn">Like <3</button>
+  </div>
+    `
+    toyCollection.append(card)
+  }
+
+  renderAllToys()
+
 });
